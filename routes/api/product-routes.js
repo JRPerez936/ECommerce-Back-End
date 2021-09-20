34c7d5cd14e0e4d6f94ecdd3d33e12b1865b01be
Justin Perez <perezjrp936@gmail.com>
2021-09-19 23:08:39 -0400

    added delete route to product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -122,7 +122,20 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where:{
+      id: req.params.id
+    }
+  }).then(dbProduct =>{
+    if(!dbProduct){
+      res.status(404).json({message: 'No Products Found'});
+      return;
+    }
+    res.json(dbProduct);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
