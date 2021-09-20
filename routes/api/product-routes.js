4885a50cedfa036e0f0903dd18357824b19ac5b4
Justin Perez <perezjrp936@gmail.com>
2021-09-19 22:56:55 -0400

    added error catch

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -17,7 +17,8 @@ router.get('/', (req, res) => {
   }).then(dbProduct => res.json(dbProduct))
   .catch(err =>{
     console.log(err);
-  })
+    res.status(500).json(err);
+  });
 });
 
 // get one product
